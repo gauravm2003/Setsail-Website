002004c9022e3b957f3cc8e98f8d81d709c09174
Gaurav Melwani <2022pietcagourav023@poornima.org>
2025-10-07 01:27:21 +0530

    Connected To Backend
    Connected To Backend

--- Frontend/src/pages/Destination.jsx
+++ Frontend/src/pages/Destination.jsx
@@ -12,7 +12,8 @@ function Destination() {
     useEffect(() => {
         const fetchTrips = async () => {
             try {
-                const res = await fetch("http://localhost:5000/api/trips");
+                // const res = await fetch("http://localhost:5000/api/trips");
+                const res = await fetch("https://setsail-backend.onrender.com/api/trips");
                 const data = await res.json();
                 setTrips(data);
             } catch (err) {
