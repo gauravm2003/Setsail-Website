3a2fa23878477265057dc33cf59c9a0f1ff47e38
Gaurav Melwani <2022pietcagourav023@poornima.org>
2025-10-07 00:09:46 +0530

    Admin Panel Updated
    Admin Panel Updated

--- Frontend/src/Admin/AddNewTrip.jsx
+++ Frontend/src/Admin/AddNewTrip.jsx
@@ -31,7 +31,8 @@ function AddNewTrip() {
         setMessage("");
 
         try {
-            const res = await fetch("http://localhost:5000/api/trips", {
+            // const res = await fetch("http://localhost:5000/api/trips", {
+            const res = await fetch("https://setsail-backend.onrender.com/api/trips", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
