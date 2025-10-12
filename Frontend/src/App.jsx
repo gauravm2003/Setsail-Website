b7a7f8bb532c9a247167210b89813c3d719fc840
Gaurav Melwani <2022pietcagourav023@poornima.org>
2025-10-13 02:16:39 +0530

    Updated
    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,78 +1,78 @@
-import 'react-toastify/dist/ReactToastify.css';
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css';
+// import { Routes, Route, Navigate } from 'react-router-dom'
+// import { ToastContainer } from 'react-toastify'
 
-// Pages..
-import './App.css'
-import Home from './Home/Home'
-// import About from './pages/About'
-import Destination from './pages/Destination'
-import CreateTrip from './pages/CreateTrip'
-import ViewTrip from './view-trip/[tripID]'
-import MyTrips from './my-trips'
-import TripPage from './trips/TripPage';
-import Tarragona from './trips/Tarragona';
-import Madrid from './trips/Madrid';
-import Barcelona from './trips/Barcelona';
-import Spain from './trips/Spain';
-import Admin from './Admin/Admin';
-import Confirm from './trips/Confirm'
-import AddNewTrip from './Admin/AddNewTrip'
+// // Pages..
+// import './App.css'
+// import Home from './Home/Home'
+// // import About from './pages/About'
+// import Destination from './pages/Destination'
+// import CreateTrip from './pages/CreateTrip'
+// import ViewTrip from './view-trip/[tripID]'
+// import MyTrips from './my-trips'
+// import TripPage from './trips/TripPage';
+// import Tarragona from './trips/Tarragona';
+// import Madrid from './trips/Madrid';
+// import Barcelona from './trips/Barcelona';
+// import Spain from './trips/Spain';
+// import Admin from './Admin/Admin';
+// import Confirm from './trips/Confirm'
+// import AddNewTrip from './Admin/AddNewTrip'
 
 
-// Layouts..
-import LayoutWithNavbar from './layouts/LayoutWithNavbar';
-import LayoutWithoutNavbar from './layouts/LayoutWithoutNavbar';
+// // Layouts..
+// import LayoutWithNavbar from './layouts/LayoutWithNavbar';
+// import LayoutWithoutNavbar from './layouts/LayoutWithoutNavbar';
 
-function App() {
+// function App() {
 
-    return (
-        <>
+//     return (
+//         <>
 
-            <Routes>
-                <Route element={<LayoutWithNavbar />} >
+//             <Routes>
+//                 <Route element={<LayoutWithNavbar />} >
 
-                    <Route path="/" element={<Home />} />
+//                     <Route path="/" element={<Home />} />
 
-                    {/* <Route path="/about" element={<About />} /> */}
+//                     {/* <Route path="/about" element={<About />} /> */}
 
-                    <Route path="/destination" element={<Destination />} />
+//                     <Route path="/destination" element={<Destination />} />
 
-                    <Route path="/ai-trip-planner" element={<CreateTrip />} />
+//                     <Route path="/ai-trip-planner" element={<CreateTrip />} />
 
-                    <Route path="/view-trip/:tripID" element={<ViewTrip />} />
+//                     <Route path="/view-trip/:tripID" element={<ViewTrip />} />
 
-                    <Route path="/my-trips" element={<MyTrips />} />
+//                     <Route path="/my-trips" element={<MyTrips />} />
 
-                    <Route path="/tarragona" element={<Tarragona />} />
+//                     <Route path="/tarragona" element={<Tarragona />} />
                     
-                    <Route path="/spain" element={<Spain />} />
+//                     <Route path="/spain" element={<Spain />} />
                     
-                    <Route path="/madrid" element={<Madrid />} />
+//                     <Route path="/madrid" element={<Madrid />} />
 
-                    <Route path="/barcelona" element={<Barcelona />} />
+//                     <Route path="/barcelona" element={<Barcelona />} />
 
-                    <Route path="/trip/:idOrSlug" element={<TripPage />} />
+//                     <Route path="/trip/:idOrSlug" element={<TripPage />} />
 
-                    <Route path="/confirm" element={<Confirm />} />
+//                     <Route path="/confirm" element={<Confirm />} />
 
-                </Route>
-            </Routes>
+//                 </Route>
+//             </Routes>
 
-            <Routes>
-                <Route element={<LayoutWithoutNavbar />} >
+//             <Routes>
+//                 <Route element={<LayoutWithoutNavbar />} >
 
-                    <Route path="/admin" element={<Admin />} />
+//                     <Route path="/admin" element={<Admin />} />
 
-                    <Route path="/add-new-trip" element={<AddNewTrip />} />
+//                     <Route path="/add-new-trip" element={<AddNewTrip />} />
 
-                </Route>
-            </Routes>
+//                 </Route>
+//             </Routes>
 
-            <ToastContainer position="top-right" autoClose={2000} />
+//             <ToastContainer position="top-right" autoClose={2000} />
 
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
-export default App
+// export default App
